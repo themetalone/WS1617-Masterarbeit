c58404dd48ee95a9f63a096df0357c2383a13992
Steffen Holzer <steffenbholzer@googlemail.com>
2016-11-09 18:43:35 +0100

    current state of work

---
+++ Simulation/PS-Simulation/src/main/java/com/github/themetalone/psSimulation/utils/clock/Clock.java
@@ -0,0 +1,13 @@
+package com.github.themetalone.psSimulation.utils.clock;
+
+import java.util.Observable;
+
+/**
+ * The generic time keeper for discrete processes
+ * 
+ * @author themetalone
+ *
+ */
+public class Clock extends Observable {
+
+}
